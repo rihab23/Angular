c7e23dfb3e77202b3312893a7b20822cf1231e06
RihabK <rihab.kandil@esprit.tn>
2016-04-11 23:41:11 +0100

    Initial commit

---
+++ nrgnetwork/scripts/app.js
@@ -0,0 +1,22 @@
+angular.module('NetworkWebApp', [
+  'NetworkWebApp.services',
+  'NetworkWebApp.controllers',
+  'ngRoute'
+  ])
+.config(function ($routeProvider, $httpProvider) {
+    // Pages
+    $routeProvider.when("/home", {templateUrl: "partials/index.html", controller: "HomeCtrl"});
+    $routeProvider.when("/activity", {templateUrl: "partials/activity.html", controller: "ActivityCtrl"});
+    $routeProvider.when("/challenges", {templateUrl: "partials/challenges.html", controller: "ChallengesCtrl"});
+    $routeProvider.when("/myportfolio", {templateUrl: "partials/myportfolio.html", controller:"MyportfolioCtrl"});
+    $routeProvider.when("/addwork", {templateUrl: "partials/addwork.html", controller: "AddworkCtrl"});
+    $routeProvider.when("/portfolio", {templateUrl: "partials/portfolio.html", controller: "PortfolioCtrl"});
+    $routeProvider.when("/discover", {templateUrl: "partials/discover.html", controller: "DiscoverCtrl"});
+    // discover
+    $routeProvider.when("/discover/people", {templateUrl: "partials/people.html", controller: "PeopleCtrl"});
+    $routeProvider.when("/discover/gallerie", {templateUrl: "partials/gallery.html", controller:"GallerieCtrl"});
+    $routeProvider.when("/discover/explore", {templateUrl: "partials/search.html", controller: "SearchCtrl"});
+  
+  $routeProvider.otherwise({redirectTo: '/home'});
+});
+
---
+++ nrgnetwork/scripts/controllers/controllers.js
@@ -0,0 +1,252 @@
+var app = angular.module('NetworkWebApp.controllers', []);
+
+
+/**
+ * Controls all other Pages
+ */
+app.controller('HomeCtrl', function (/* $scope, $location, $http */) {
+  console.log("Page Controller reporting for duty.");
+  	//MIX UP
+	if($('#container-mix').length) {
+		$('#container-mix').mixItUp(
+			{
+				animation: {
+					duration: 400,
+					effects: 'fade translateZ(-360px) stagger(34ms)',
+					easing: 'ease'
+				}
+			}
+		);};
+			$(".be-drop-down").on("click" ,function(){
+		$(this).toggleClass("be-dropdown-active");
+		$(this).find(".drop-down-list").stop().slideToggle();
+	});
+	$(".drop-down-list li").on("click", function(){
+		var new_value = $(this).find("a").text();
+		$(this).parent().parent().find(".be-dropdown-content").text(new_value);
+			return false;
+	});
+
+
+});
+/**
+ * Controls the Activity
+ */
+app.controller('ActivityCtrl', function (/* $scope, $location, $http */) {
+  console.log("Activity Controller reporting for duty.");
+    	//MIX UP
+	if($('#container-mix').length) {
+		$('#container-mix').mixItUp(
+			{
+				animation: {
+					duration: 400,
+					effects: 'fade translateZ(-360px) stagger(34ms)',
+					easing: 'ease'
+				}
+			}
+		);};
+			$(".be-drop-down").on("click" ,function(){
+		$(this).toggleClass("be-dropdown-active");
+		$(this).find(".drop-down-list").stop().slideToggle();
+	});
+	$(".drop-down-list li").on("click", function(){
+		var new_value = $(this).find("a").text();
+		$(this).parent().parent().find(".be-dropdown-content").text(new_value);
+			return false;
+	});
+
+});
+
+/**
+ * Controls the Discover
+ */
+app.controller('DiscoverCtrl', function (/* $scope, $location, $http */) {
+  console.log("Discover Controller reporting for duty.");
+  	//MIX UP
+	if($('#container-mix').length) {
+		$('#container-mix').mixItUp(
+			{
+				animation: {
+					duration: 400,
+					effects: 'fade translateZ(-360px) stagger(34ms)',
+					easing: 'ease'
+				}
+			}
+		);};
+			$(".be-drop-down").on("click" ,function(){
+		$(this).toggleClass("be-dropdown-active");
+		$(this).find(".drop-down-list").stop().slideToggle();
+	});
+	$(".drop-down-list li").on("click", function(){
+		var new_value = $(this).find("a").text();
+		$(this).parent().parent().find(".be-dropdown-content").text(new_value);
+			return false;
+	});
+
+});
+/**
+ * Controls the Challenges
+ */
+app.controller('ChallengesCtrl', function (/* $scope, $location, $http */) {
+  console.log("Challenges Controller reporting for duty.");
+  	//MIX UP
+	if($('#container-mix').length) {
+		$('#container-mix').mixItUp(
+			{
+				animation: {
+					duration: 400,
+					effects: 'fade translateZ(-360px) stagger(34ms)',
+					easing: 'ease'
+				}
+			}
+		);};
+			$(".be-drop-down").on("click" ,function(){
+		$(this).toggleClass("be-dropdown-active");
+		$(this).find(".drop-down-list").stop().slideToggle();
+	});
+	$(".drop-down-list li").on("click", function(){
+		var new_value = $(this).find("a").text();
+		$(this).parent().parent().find(".be-dropdown-content").text(new_value);
+			return false;
+	});
+
+});
+/**
+ * Controls the Myportfolio
+ */
+app.controller('MyportfolioCtrl', function (/* $scope, $location, $http */) {
+  console.log("Myportfolio Controller reporting for duty.");
+  	//MIX UP
+	if($('#container-mix').length) {
+		$('#container-mix').mixItUp(
+			{
+				animation: {
+					duration: 400,
+					effects: 'fade translateZ(-360px) stagger(34ms)',
+					easing: 'ease'
+				}
+			}
+		);};
+			$(".be-drop-down").on("click" ,function(){
+		$(this).toggleClass("be-dropdown-active");
+		$(this).find(".drop-down-list").stop().slideToggle();
+	});
+	$(".drop-down-list li").on("click", function(){
+		var new_value = $(this).find("a").text();
+		$(this).parent().parent().find(".be-dropdown-content").text(new_value);
+			return false;
+	});
+
+});
+/**
+ * Controls the Addwork
+ */
+app.controller('AddworkCtrl', function (/* $scope, $location, $http */) {
+  console.log("Addwork Controller reporting for duty.");
+  	//MIX UP
+	if($('#container-mix').length) {
+		$('#container-mix').mixItUp(
+			{
+				animation: {
+					duration: 400,
+					effects: 'fade translateZ(-360px) stagger(34ms)',
+					easing: 'ease'
+				}
+			}
+		);};
+			$(".be-drop-down").on("click" ,function(){
+		$(this).toggleClass("be-dropdown-active");
+		$(this).find(".drop-down-list").stop().slideToggle();
+	});
+	$(".drop-down-list li").on("click", function(){
+		var new_value = $(this).find("a").text();
+		$(this).parent().parent().find(".be-dropdown-content").text(new_value);
+			return false;
+	});
+
+});
+/**
+ * Controls the Portfolio
+ */
+app.controller('PortfolioCtrl', function (/* $scope, $location, $http */) {
+  console.log("Portfolio Controller reporting for duty.");
+  	//MIX UP
+	if($('#container-mix').length) {
+		$('#container-mix').mixItUp(
+			{
+				animation: {
+					duration: 400,
+					effects: 'fade translateZ(-360px) stagger(34ms)',
+					easing: 'ease'
+				}
+			}
+		);};
+			$(".be-drop-down").on("click" ,function(){
+		$(this).toggleClass("be-dropdown-active");
+		$(this).find(".drop-down-list").stop().slideToggle();
+	});
+	$(".drop-down-list li").on("click", function(){
+		var new_value = $(this).find("a").text();
+		$(this).parent().parent().find(".be-dropdown-content").text(new_value);
+			return false;
+	});
+
+});
+/**
+ * Controls the People
+ */
+app.controller('PeopleCtrl', function (/* $scope, $location, $http */) {
+  console.log("People Controller reporting for duty.");
+  	//MIX UP
+	if($('#container-mix').length) {
+		$('#container-mix').mixItUp(
+			{
+				animation: {
+					duration: 400,
+					effects: 'fade translateZ(-360px) stagger(34ms)',
+					easing: 'ease'
+				}
+			}
+		);};
+
+});
+/**
+ * Controls the Gallerie
+ */
+app.controller('GallerieCtrl', function (/* $scope, $location, $http */) {
+  console.log("Gallerie Controller reporting for duty.");
+  
+
+});
+/**
+ * Controls the Search
+ */
+app.controller('SearchCtrl', function (/* $scope, $location, $http */) {
+  console.log("Search Controller reporting for duty.");
+  	//MIX UP
+	if($('#container-mix').length) {
+		$('#container-mix').mixItUp(
+			{
+				animation: {
+					duration: 400,
+					effects: 'fade translateZ(-360px) stagger(34ms)',
+					easing: 'ease'
+				}
+			}
+		);};
+			$(".be-drop-down").on("click" ,function(){
+		$(this).toggleClass("be-dropdown-active");
+		$(this).find(".drop-down-list").stop().slideToggle();
+	});
+	$(".drop-down-list li").on("click", function(){
+		var new_value = $(this).find("a").text();
+		$(this).parent().parent().find(".be-dropdown-content").text(new_value);
+			return false;
+	});
+
+});
+ 
+
+
+      									
+      	
